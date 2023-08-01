c73f88a04138b3e0f467e5cfb2c1ef9938fa84ff
임지훈 <zhoon95@gmail.com>
2023-08-01 14:43:38 +0900

    Water

---
+++ Water/README.md
@@ -0,0 +1,6 @@
+# Water
+
+### CodeSandBox
+
+JavaScript : (https://codesandbox.io/s/water-87wxhx?file=/index.html)[https://codesandbox.io/s/water-87wxhx?file=/index.html] \
+React : ()[]
---
+++ Water/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrap, GlobalStyle } from "./styles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Wrap></Wrap>
+    </>
+  );
+};
+
+export default App;
