9eab841f6a1f3157909b72d86acaa2b9a8e2446a
임지훈 <zhoon95@gmail.com>
2023-10-12 17:41:43 +0900

    PULL

--- src/pages/About/style.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrap = styled.div`
-  min-height: 100vh;
-  background: #2f323f;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
