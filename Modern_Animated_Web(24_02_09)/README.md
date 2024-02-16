803044d15fc4085ae4e56adc6b44ba6b4ee920e8
MontaKr <zhoon95@gmail.com>
2024-02-16 17:13:44 +0900

    MODERN

---
+++ Modern_Animated_Web(24_02_09)/README.md
@@ -0,0 +1,7 @@
+# Modern_Animated_Web
+
+## Used Stack
+React / Vite / Tailwindcss / Framer-Motion / React-Icons
+
+### CodeSandBox
+React : []
---
+++ Modern_Animated_Web(24_02_09)/src/components/LandingPage.jsx
@@ -0,0 +1,58 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { FaArrowUpLong } from "react-icons/fa6";
+
+function LandingPage() {
+  return (
+    <div
+      data-scroll
+      data-scroll-speed="-.3"
+      data-scroll-section
+      className="w-full h-screen bg-zinc-900 pt-1"
+    >
+      <div className="textstructure mt-52 px-20">
+        {["We Create", "Eye Opening", "Preseantations"].map((item, index) => {
+          return (
+            <div className="masker">
+              <div className="w-fit flex items-end overflow-hidden">
+                {index === 1 && (
+                  <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: "9vw" }}
+                    transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
+                    className="mr-[1vw] w-[8vw] rounded-md h-[4.5vw] relative -top-[0.5vw] bg-green-500"
+                  ></motion.div>
+                )}
+                <h1 className="pt-[2vw] -mb-[1vw] text-[6.5vw] uppercase leading-[.75] h-full tracking-tighter font-founders-grotesk font-bold">
+                  {item}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20">
+        {[
+          "For public and private companies",
+          "From the firtst pitch to IPO",
+        ].map((item, index) => (
+          <p className="text-md font-light tracking-tight leading-none">
+            {item}
+          </p>
+        ))}
+        <div className="start flex items-center gap-5">
+          <div className="px-5 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full">
+            start the project
+          </div>
+          <div className="w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full">
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
